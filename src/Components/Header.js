5bc37eb1d48daace28ff8b94a1ed271d0020cfea
Hung Nguyen <hdnguyen527@gmail.com>
2021-12-27 10:46:25 -0800

    added app bar header and useHistory hook for navigation. dark theme

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,61 @@
-import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Container, createTheme, makeStyles, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flex: 1,
+        color: 'gold',
+        fontFamily: 'Montserrat',
+        fontWeight: 'bold',
+        cursor: 'pointer',
+    }
+}))
 
 const Header = () => {
+    const classes = useStyles()
+
+    const history = useHistory()
+
+    const darkTheme = createTheme({
+      palette: { 
+          primary: {
+            main: "#fff",
+          },
+          type: "dark",
+        },
+      });
+
     return (
-        <AppBar color='transparent' position='static'>
+        <ThemeProvider theme={darkTheme}>
+          <AppBar color='transparent' position='static'>
+              {/* container is used to make the component responsive when changing screen size */}
           <Container>
-            <Toolbar>
-              <Typography>
-                Coin Tracker
+              <Toolbar>
+                  {/* typography is imported from MUI and used for formatting text */}
+              <Typography 
+                  onClick={() => history.push('/')} 
+                  className={classes.title}
+                  variant='h6'
+              >
+                  Coin Tracker
               </Typography>
               <Select 
-                variant='outlined' 
-                style={{
+                  variant='outlined' 
+                  style={{
                   width: 100, 
                   height: 40, 
-                  marginLeft: 15,
-                }}
+                  marginRight: 15,
+                  }}
               >
-                <MenuItem value={'USD'}>USD</MenuItem>
-                <MenuItem value={'CAD'}>CAD</MenuItem>
+                  <MenuItem value={'USD'}>USD</MenuItem>
+                  <MenuItem value={'CAD'}>CAD</MenuItem>
               </Select>
-            </Toolbar>
+              </Toolbar>
           </Container>
-        </AppBar>
+          </AppBar>
+        </ThemeProvider>
     )
 }
 
